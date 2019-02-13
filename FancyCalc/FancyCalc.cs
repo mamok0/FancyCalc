221364aa60c682b306fcf6fd1f62d56bb89962df
Artem Kivachuk <mamokosf@gmail.com>
2019-02-13 21:22:51 +0300

    fixing mistakes

--- FancyCalc/FancyCalc.cs
+++ FancyCalc/FancyCalc.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FancyCalc
@@ -11,28 +12,47 @@ namespace FancyCalc
 
         public double Add(int a, int b)
         {
-            throw new NotImplementedException();
-            //return a + b;
+            return (double)a + b;
         }
 
 
         public double Subtract(int a, int b)
         {
-            //throw new NotImplementedException();
-            return a - b;
+            return (double)a - b;
         }
 
 
         public double Multiply(int a, int b)
         {
-            return a * b;
+            return (double)a * b;
         }
 
         //generic calc method. usage: "10 + 20"  => result 30
         public double Culculate(string expression)
         {
-            throw new NotImplementedException();
-
+            if (expression == null)
+                throw new ArgumentNullException();
+            char[] expressionsSymbols = { '+', '-', '/', '*' };
+            char expr = '0';
+            for (int i = 0; i < expression.Length; i++)
+            {
+                for(int j=0; j<expressionsSymbols.Length; j++)
+                {
+                    if (expression[i] == expressionsSymbols[j])
+                        expr = expression[i];
+                }
+            }
+            string[] numbers_string = expression.Trim().Split(expr);
+            IEnumerable<double> nums_double = numbers_string.Select(num => Convert.ToDouble(num));
+            double answer = 0;
+            switch(expr)
+            {
+                case '+': answer = nums_double.First() + nums_double.Last(); break;
+                case '-': answer = nums_double.First() - nums_double.Last(); break;
+                case '*': answer = nums_double.First() * nums_double.Last(); break;
+                case '/': answer = nums_double.First() / nums_double.Last(); break;
+            }
+            return answer;
         }
     }
 }
